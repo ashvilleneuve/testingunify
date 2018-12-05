777a9606b9004194a3d92b7bd1805439adc130db
Brydon McCluskey <brydonmccluskey@shopify.com>
2018-12-05 14:54:27 -0800

    hello world

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
