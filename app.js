a02cf0973a9d42704739f56aad25cff127cc1f12
Alexander Lamm <alexander.lamm@shopify.com>
2018-12-06 08:23:13 -0800

    skeleton only

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const request = require("request");
+// const exphbs = require('express-handlebars');
+// const request = require("request");
 
 // Internal config variables
 const app = express();
-const errorHandlers = require("./handlers/errorHandlers");
-const routes = require("./routes/index");
+// const errorHandlers = require("./handlers/errorHandlers");
+// const routes = require("./routes/index");
 
 const port = 8080;
 
-app.use(express.static(__dirname + '/public'));
-// Handlebars Middleware
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-
-app.set('view engine', 'handlebars');
-
-// app.use("/", routes);
-app.get('/', (req, res) => res.send('Hello World!'))
-
-// If that above routes didnt work, we 404 them and forward to error handler
-app.use(errorHandlers.notFound);
-
-// One of our error handlers will see if these errors are just validation errors
-app.use(errorHandlers.flashValidationErrors);
-
-// Otherwise this was a really bad error we didn't expect! Shoot eh
-if (app.get("env") === "development") {
-    /* Development Error Handler - Prints stack trace */
-    app.use(errorHandlers.developmentErrors);
-}
-
-// production error handler
-app.use(errorHandlers.productionErrors);
+// app.use(express.static(__dirname + '/public'));
+// // Handlebars Middleware
+// app.engine('handlebars', exphbs({
+//     defaultLayout: 'main'
+// }));
+//
+// app.set('view engine', 'handlebars');
+//
+// // app.use("/", routes);
+// app.get('/', (req, res) => res.send('Hello World!'))
+//
+// // If that above routes didnt work, we 404 them and forward to error handler
+// app.use(errorHandlers.notFound);
+//
+// // One of our error handlers will see if these errors are just validation errors
+// app.use(errorHandlers.flashValidationErrors);
+//
+// // Otherwise this was a really bad error we didn't expect! Shoot eh
+// if (app.get("env") === "development") {
+//     /* Development Error Handler - Prints stack trace */
+//     app.use(errorHandlers.developmentErrors);
+// }
+//
+// // production error handler
+// app.use(errorHandlers.productionErrors);
 
 // Setup http server
 app.listen(port, (err) => {
@@ -44,4 +44,4 @@ app.listen(port, (err) => {
 })
 
 // done! we export it so we can start the site in start.js
-module.exports = app;
+// module.exports = app;
